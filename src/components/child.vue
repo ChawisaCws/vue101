93db15f64f3805b78099539dd07cb31c4867405f
Chawisa <590107030017@dpu.ac.th>
2019-06-14 19:09:45 +0700

    qeq

---
+++ src/components/child.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input type="text" v-model="messageChild" />
+        <button @click.prevent="submit">Submit</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            messageChild: 'child message'
+        }
+    },
+    methods:{
+        submit: function(){
+            this.$emit('input',this.messageChild)
+        }
+    }
+
+}
+</script>
